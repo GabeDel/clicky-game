3b9999add04111ae12c933862d911d8dc998b71e
Gabriel Delgado <gabrield2323@gmail.com>
2019-05-05 22:58:41 -0600

    main part of the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components./Navbar";
+import Jumbotron from "./component./Jumbotron";
+import Card from "./component./Card";
+import Footer from "./component./Footer";
+import pokemon from "pokemon.json";
 
-function App() {
+class App extends Component {
+  state = {
+    pokemon,
+    clickedPokemon: [],
+    score: 0
+  };
+
+
+  imageClick = event => {
+    const currentPokemon = event.target.alt;
+    const PokemonAlreadyClicked =
+      this.state.clickedPokemon.indexOf(currentPokemon) > -1;
+
+
+    if (PokemonAlreadyClicked) {
+      this.setState({
+        Pokemon: this.state.pokemon.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedPokemon: [],
+        score: 0
+      });
+        alert("Lol you lose. Want to lose again?");
+
+    } else {
+      this.setState(
+        {
+          pokemon: this.state.pokemon.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedPokemon: this.state.clickedPokemon.concat(
+            currentPokemon
+          ),
+          score: this.state.score + 1
+        },
+       
+        () => {
+          if (this.state.score === 12) {
+            alert("You Win!");
+            this.setState({
+              pokemon: this.state.pokemon.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedPokemon: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  };
+
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar 
+        score={this.state.score}
+      />
+      <Jumbotron />
+      <div className="wrapper">
+        {this.state.pokemon.map(pokemon => (
+          <Card
+            imageClick={this.imageClick}
+            id={pokemon.id}
+            key={pokemon.id}
+            image={pokemon.image}
+          />
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/components/Jumbotron/jumbotron.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "Jumbotron.css";
+
+const Jumbotron = () => (
+	<header className = "header">
+		<h1>Pokemon Clicky Game</h1>
+		<h2>Click a Pokemon to score a point, but dont click the same pokemon twice because you'll lose. Try to click all 12 pokemon to win!</h2>
+	</header>
+);
+export default Jumbotron;
